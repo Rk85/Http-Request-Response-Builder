e46752dfae84e205ea7ba5d5e4e65ce19e911767
Rk85 <rachandkrishnan@gmail.com>
2013-12-24 03:06:05 +0530

    Global Config files for the tester

---
+++ configs/client_config.py
@@ -0,0 +1,11 @@
+"""
+	Global Config File for the HTTP Client
+"""
+
+EOL1 = '\n\n'
+EOL2 = '\n\r\n'
+PARALLEL_CLIENTS = 1
+TOT_REQUESTS = 2
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8081
+READ_BLOCK_SIZE = 1024
